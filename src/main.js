8225ca34aeaae7385b88bb5c6bc3b6d335d6619d
Frank Matranga <thefrankmatranga@gmail.com>
2018-08-21 21:12:32 -0400

    SemanticUI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import SuiVue from 'semantic-ui-vue';
+
+import 'semantic-ui-css/semantic.min.css';
+
+Vue.use(SuiVue);
 
 Vue.config.productionTip = false;
 
@@ -9,4 +14,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
